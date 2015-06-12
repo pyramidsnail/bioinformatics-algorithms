182d52d7aa65e11d709bff347add8dcefb6cc392
pyramidsnail <sherry.bim@gmail.com>
2015-06-12 08:25:44 +0200

    :)

--- proteomics.py
+++ proteomics.py
@@ -19,7 +19,9 @@ mass = {'G':57,
         'F':147,
         'R':156,
         'Y':163,
-        'W':186}
+        'W':186,
+        'X':4,
+        'Z':5}
 
 def construct_graph(spectrum):
     res = {}
@@ -45,7 +47,29 @@ def find_path(graph, start, end, path):
             return find_path(graph, node, end, path)
     return None
 
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in set(graph[vertex]) - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
 
+def idealSpectrum(seq):
+    res = [0]
+    pre = 0
+    for i in xrange(len(seq)):
+        pre += mass[seq[i]]
+        res.append(pre)
+    suf = 0
+    seq_re = seq[::-1]
+    for i in xrange(len(seq_re)):
+        suf += mass[seq_re[i]]
+        res.append(suf)
+    return list(set(res))
+        
 def decoding(spectrum):
     res = construct_graph(spectrum)
     graph = {}
@@ -55,13 +79,42 @@ def decoding(spectrum):
         graph[key[0]].append(key[1])
     start = 0
     end = max(spectrum)
-    path = []
-    find_path(graph, start, end, path)
-    seq = ''
-    for i in xrange(1, len(path)):
-        seq += mass.keys()[mass.values().index(path[i]-path[i-1])]
-    return seq
 
+    paths = bfs_paths(graph, start, end)
+    for path in paths:
+        seq = ''
+        for i in xrange(1, len(path)):
+            seq += mass.keys()[mass.values().index(path[i]-path[i-1])]
+        if sorted(idealSpectrum(seq)) == sorted(spectrum):
+            return seq
+
+
+def peptide_vector(seq):
+    prefix = []
+    total = 0
+    for i in seq:
+        total += mass[i]
+        prefix.append(total)
+    # prefix.sort()
+    res = []
+    for i in xrange(max(prefix)):
+        if i+1 in prefix:
+            res.append(1)
+        else:
+            res.append(0)
+    return res
+
+
+def peptide(vector):
+    seq = ''
+    first = -1
+    for i in xrange(len(vector)):
+        if int(vector[i]) == 1:
+            seq += mass.keys()[mass.values().index(i-first)]
+            first = i
+
+    return seq
+        
 
 if __name__ == '__main__':
     # f = open('test', 'r')
@@ -73,8 +126,17 @@ if __name__ == '__main__':
     #     print s
                 
 
-    f = open('test', 'r')
-    spectrum = f.readline().strip().split()
-    spectrum = [int(x) for x in spectrum]
-    print decoding(spectrum)
+    # f = open('test', 'r')
+    # spectrum = f.readline().strip().split()
+    # spectrum = [int(x) for x in spectrum]
+    # print decoding(spectrum)
+
+    # f = open('test', 'r')
+    # seq = f.readline().strip()
+    # res = peptide_vector(seq)
+    # for i in res:
+    #     print i,
                 
+   f = open('test', 'r')
+   vector = f.readline().strip().split()
+   print peptide(vector)
