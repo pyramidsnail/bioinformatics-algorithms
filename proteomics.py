996ed3acfcb31f2afe3c2abc57f666a0f396dcb0
pyramidsnail <sherry.bim@gmail.com>
2015-06-25 16:51:31 +0200

    :)

--- proteomics.py
+++ proteomics.py
@@ -325,8 +325,56 @@ def spectral_dic_prob(spectrum, threshold, max_score):
     return total
 
 
+def spectral_alignment(seq, spectrum, threshold):
+    spectrum.insert(0,0)    
+    score = dict((i,dict((j,[-float("inf")]*len(spectrum)) for j in xrange(weight(seq)+1) )) for i in xrange(0, threshold+1))
+    score[0][0][0] = 0
+    for i in xrange(1,len(seq)+1):
+        for j in xrange(len(spectrum)):
+            for k in xrange(threshold+1):
+                if k==0 and weight(seq[:i])-mass[seq[i-1]]>=0 and j-mass[seq[i-1]]>=0:
+                    score[k][weight(seq[:i])][j] = spectrum[j]+score[k][weight(seq[:i])-mass[seq[i-1]]][j-mass[seq[i-1]]]
+                elif k>0 and weight(seq[:i])-mass[seq[i-1]]>=0 and j-mass[seq[i-1]]>=0:
+                    score[k][weight(seq[:i])][j] = spectrum[j]+max(score[k][weight(seq[:i])-mass[seq[i-1]]][j-mass[seq[i-1]]],
+                                                       max(score[k-1][weight(seq[:i])-mass[seq[i-1]]][:j]))
+
+                elif k>0 and weight(seq[:i])-mass[seq[i-1]]>=0 and j>0:
+                    score[k][weight(seq[:i])][j] = spectrum[j]+max(score[k-1][weight(seq[:i])-mass[seq[i-1]]][:j])
+
+    #####  trace back the path
+    res = ''
+    max_score = 0
+    layer = 0
+    col = len(spectrum)-1
+    for i in xrange(threshold+1):
+        if score[i][weight(seq)][len(spectrum)-1]>max_score:
+            max_score = score[i][len(seq)][len(spectrum)-1]
+            layer = i
+            
+    for i in range(len(seq), 0, -1):
+        if col-mass[seq[i-1]]>=0 and score[layer][weight(seq[:i])][col] == spectrum[col]+score[layer][weight(seq[:i])-mass[seq[i-1]]][col-mass[seq[i-1]]]:
+            layer -= 1
+            col = col-mass[seq[i-1]]
+            res = seq[i-1]+res
+            
+            
+            
+        elif score[layer][weight(seq[:i])][col] == spectrum[col]+max(score[layer-1][weight(seq[:i])-mass[seq[i-1]]][:col]):
+            part = col-score[layer-1][weight(seq[:i])-mass[seq[i-1]]][:col].index((max(score[layer-1][weight(seq[:i])-mass[seq[i-1]]][:col])))-mass[seq[i-1]]
+            col = score[layer-1][weight(seq[:i])-mass[seq[i-1]]][:col].index((max(score[layer-1][weight(seq[:i])-mass[seq[i-1]]][:col])))
+            if part>0:
+                res = seq[i-1]+'(+'+str(part) + ')' + res
+            else:
+                res = seq[i-1]+'('+str(part) + ')' + res
+            layer -= 1
+
+
+    return res 
+
 
     
+                                                       
+            
     
 if __name__ == '__main__':
     # f = open('test', 'r')
@@ -374,10 +422,18 @@ if __name__ == '__main__':
    #     print i
 
 
+   # f = open('test', 'r')
+   # lines = f.readlines()
+   # spectrum = [int(x) for x in lines[0].strip().split()]
+   # threshold = int(lines[1])
+   # max_score = int(lines[2])
+   # # print spectral_dic_size(spectrum, threshold, max_score)
+   # print spectral_dic_prob(spectrum, threshold, max_score)
+
+
    f = open('test', 'r')
    lines = f.readlines()
-   spectrum = [int(x) for x in lines[0].strip().split()]
-   threshold = int(lines[1])
-   max_score = int(lines[2])
-   # print spectral_dic_size(spectrum, threshold, max_score)
-   print spectral_dic_prob(spectrum, threshold, max_score)
+   seq = lines[0].strip()
+   spectrum = [int(x) for x in lines[1].strip().split()]
+   threshold = int(lines[2])
+   print spectral_alignment(seq, spectrum, threshold)
